45d57b7c3594d784db636c0d82b7fe4cba9208ac
Lilly <128197764+Lillinator@users.noreply.github.com>
2026-04-23 08:50:46 -0700

    Update and rename add-mobile-auth-button.js to add-combined-auth-button.js

--- javascripts/discourse/initializers/add-combined-auth-button.js
+++ javascripts/discourse/initializers/add-combined-auth-button.js
@@ -1,8 +1,8 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
-import MobileAuthButton from "../components/mobile-auth-button";
+import MobileAuthButton from "../components/combined-auth-button";
 
 export default {
-  name: "add-mobile-auth-button",
+  name: "add-combined-auth-button",
   initialize() {
     withPluginApi("1.1.0", (api) => {
       api.renderInOutlet("before-header-panel", MobileAuthButton, {
