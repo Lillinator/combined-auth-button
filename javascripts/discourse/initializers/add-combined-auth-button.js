6543f83e98048bb4b9b66841dc9223325d11d9f2
Lilly <128197764+Lillinator@users.noreply.github.com>
2026-04-23 08:55:08 -0700

    Update add-combined-auth-button.js

--- javascripts/discourse/initializers/add-combined-auth-button.js
+++ javascripts/discourse/initializers/add-combined-auth-button.js
@@ -1,12 +1,12 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
-import MobileAuthButton from "../components/combined-auth-button";
+import CombinedAuthButton from "../components/combined-auth-button"; 
 
 export default {
   name: "add-combined-auth-button",
   initialize() {
     withPluginApi("1.1.0", (api) => {
-      api.renderInOutlet("before-header-panel", MobileAuthButton, {
-        attributes: { class: "mobile-auth-button-wrapper" }
+      api.renderInOutlet("before-header-panel", CombinedAuthButton, { 
+        attributes: { class: "combined-auth-button-wrapper" } 
       });
     });
   },
